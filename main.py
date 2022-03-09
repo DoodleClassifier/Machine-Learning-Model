c348551f69fee87fa1136ee1a7e4f90118eae4cb
Jason Dykstra <jasondykstra2000@gmail.com>
2022-03-08 22:01:53 -0800

    added more data and confusion matrix visualization

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from sklearn.ensemble import RandomForestClassifier as RFC;
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import *
 import numpy as np
 import pandas as pd
 from os.path import exists
+import seaborn as sns
+import matplotlib.pyplot as plt 
+from matplotlib.ticker import MultipleLocator    
 
 objects = {
     0: "Bowtie",
@@ -9,7 +14,14 @@ objects = {
     2: "Crown",
     3: "EiffelTower",
     4: "HotAirBalloon",
-    5: "HousePlant"
+    5: "HousePlant",
+    6: "Bed",
+    7: "Cat",
+    8: "Couch",
+    9: "Dog",
+    10: "Hand",
+    11: "Hat",
+    12: "Tractor"
 }
 
 data = pd.DataFrame()
@@ -26,24 +38,48 @@ for object in objects:
 
     # Add labels to data
     temp = pd.DataFrame(object_data)
-    temp["Label"] = objects[object]
+    temp["Label"] = object
     
     # Append object data to main dataframe
     data = data.append(temp, ignore_index=True)
 
-print(data)
+
+# Train test validation split
+x_train, x_test, y_train, y_test = train_test_split(data.loc[:, data.columns != "Label"], data["Label"], test_size=0.33, random_state=69)
+
+model = RFC(n_estimators=100, max_depth=None, random_state=420)
+
+model.fit(x_train, y_train)
+
+predictions = model.predict(x_test)
+
+def show_confusion_matrix():
+    # Create labels
+    labels = list(objects.keys())
+
+    # Initialize confusion matrix
+    cm = confusion_matrix(y_true=y_test, y_pred=predictions, labels=labels)
+    print(cm)
+
+    # Initialize figure
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    cax = ax.matshow(cm)
+    plt.title('Confusion matrix of the classifier')
+    
+    fig.colorbar(cax)
+    
+    # "If you have more than a few categories, Matplotlib decides to label the axes incorrectly - you have to force it to label every cell." - https://stackoverflow.com/questions/19233771/sklearn-plot-confusion-matrix-with-labels
+    ax.xaxis.set_major_locator(MultipleLocator(1))
+    ax.yaxis.set_major_locator(MultipleLocator(1))
+    ax.set_xticklabels([''] + list(objects.values()))
+    ax.set_yticklabels([''] + list(objects.values()))
+    ax.tick_params(labelrotation=45)
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+
+    # Draw figure
+    plt.show()
 
 
-
-
-
-# X,     Y
-# [784], eiffel tower
-# [784], book
-
-# Split into x-train/test, and y-train/test
-
-# Fit to model
-
-# Win
-# print(data[0])
+# show_confusion_matrix()
--- resize.py
+++ resize.py
@@ -3,10 +3,10 @@ from os import walk
 
 # This file will automatically go into a directory called "raw_data" and convert all the numpy files to 1000 entries and save them in the "data" directory. The "raw_data" directory must be created locally as it is part of the .gitignore so I don't accidentally upload a gigabyte of data to github.
 
-filenames = next(walk("./raw_data/"), (None, None, []))[2]  # [] if no file
+filenames = next(walk("./DoodleClassifierModel/raw_data/"), (None, None, []))[2]  # [] if no file
 
 for file in filenames:
-    data = np.load(f"./raw_data/{file}")[:1000]
-    np.save(f"./data/{file}", data)
+    data = np.load(f"./DoodleClassifierModel/raw_data/{file}")[:1000]
+    np.save(f"./DoodleClassifierModel/data/{file}", data)
 
 print(f"Found and resized {len(filenames)} files!")
